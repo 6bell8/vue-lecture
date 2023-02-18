f1461aeff7412dca5bf51e5917d07e3000b4b2ca
par jin sung <parkgutime@gmail.com>
2023-02-18 13:44:24 +0900

    뷰 복습

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
   // data 프로퍼티 내를 감시하는 기능 react (useEffect와 얼핏 비슷, 리렌더링방지)
   watch: {
     // watch의(감시자) 진짜 주요한 기능 동적인 요소를 캐치해서 변화시킴
+    // 내부에서 바뀌는 작업이 있을 때 사용하는 것이 좋다
     counter(value) {
       if (value > 50) {
         const that = this;
@@ -40,6 +41,7 @@ const app = Vue.createApp({
     //   }
     // },
   },
+  // 다른 데이터에 의존하는 데이터 출력엔 연산프로퍼티를 사용
   // 어디서나 구성옵션을 끼워 넣을 수 있음
   // 연산자옵션을 하는 이유는 불필요한 리렌더링을 막고, html에서의 연산을 최소화하기 위해서
   // computed의 주요한 기능은 동적 계산을 (+) 빠르게 할 수 있음
@@ -56,6 +58,7 @@ const app = Vue.createApp({
   // 인자 값을 임의로 설정해서 html 이벤트에 삽입한다.
   // 메서드에 있는 + 연산자는 다른 메서드까지 렌더링하는 경우가 있다 때문에 computed라는 연산 프로퍼티에 담아서 사용한다.
   // 메서드의 순서는 중요하지않다
+  // 이벤트 바인딩은 항상 메서드를 사용한다
   methods: {
     outputFullname() {
       console.log("재실행 하겠습니다.");
