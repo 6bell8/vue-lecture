0c23aac531ca7578f0ba7fa9047f36002a30ab8d
par jin sung <parkgutime@gmail.com>
2023-03-01 23:11:47 +0900

    동적 method class 인자값 지정

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
   methods: {
     boxSelected(box) {
       if (box === "A") {
-        //복잡하게 토글하는 것 보다 !를 이용해서 상태를 바꿔줌
+        // 복잡하게 토글하는 것 보다 !를 이용해서 상태를 바꿔줌
+        // 인자값으로 class 지정, !this 토글
         this.boxASelected = !this.boxASelected;
       } else if (box === "B") {
         this.boxBSelected = !this.boxBSelected;
